4631fd692ff3beefc39df004ec56147a76aa9da7
youtkddnjs <youtkddnjs91@gmail.com>
2024-07-22 11:06:04 +0900

    Add Color Extensions

---
+++ voiceMemoApp/voiceMemoApp/Core/Extension/Color+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Color+extensions.swift
+//  voiceMemo
+//
+
+import SwiftUI
+
+extension Color {
+    static let mycustomBackgroundGreen = Color("customBackgroundGreen")
+    static let mycustomBlack = Color("customBlack")
+    static let mycustomCoolGray = Color("customCoolGray")
+    static let mycustomGray0 = Color("customGray0")
+    static let mycustomGray1 = Color("customGray1")
+    static let mycustomGray2 = Color("customGray2")
+    static let mycustomGreen = Color("customGreen")
+    static let mycustomIconGray = Color("customIconGray")
+    static let mycustomOrange = Color("customOrange")
+    static let mycustomSky = Color("customSky")
+    static let mycustomWhite = Color("cutomBackgroundGreen")
+}
