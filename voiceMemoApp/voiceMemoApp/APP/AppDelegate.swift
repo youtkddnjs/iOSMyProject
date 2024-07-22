1ca11d117355d274bf18983f533c0ff67ac51fbd
youtkddnjs <youtkddnjs91@gmail.com>
2024-07-22 10:41:03 +0900

    Initialization Project

---
+++ voiceMemoApp/voiceMemoApp/APP/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  voiceMemoApp
+//
+//  Created by sangwon on 7/22/24.
+//
+
+import UIKit
+
+class AppDelegate{
+    
+}
---
+++ voiceMemoApp/voiceMemoApp/APP/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemoApp
+//
+//  Created by sangwon on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+      WindowGroup {
+        OnboardingView()
+      }
+    }
+}
